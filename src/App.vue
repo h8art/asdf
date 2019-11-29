ca075b5707e62ab15c49f22f39d499c322999480
h8art <artem.gershteyn@gmail.com>
2019-11-30 00:08:44 +0300

    innit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang='pug'>
+  #app
+    toolbar
+    .main-cont
+      sidebar
+      .router
+        router-view
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Toolbar from '@/components/Toolbar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+export default {
+  components: { Toolbar, Sidebar }
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+</script>
+<style lang="sass">
+body
+  margin: 0
+  font-family: "Rubik"
+.main-cont
+  background: #f5f9ff
+  height: calc(100vh - 60px)
+  display: flex
+  .router
+    height: 100%
+    padding-top: 48px
+    box-sizing: border-box
 </style>
